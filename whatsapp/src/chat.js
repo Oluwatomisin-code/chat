214fd416f0d26124d075832a440e091f98fc0c1e
oluwatomisin-code <tomisintomori@gmail.com>
2020-12-24 04:26:16 +0100

    second commit, worked on chat functionality

--- whatsapp/src/chat.js
+++ whatsapp/src/chat.js
@@ -19,7 +19,7 @@ function Chat(){
     const [chat, setChat] = useState([])
 
     useEffect(()=>{
-        socket.on('Message', (Msg) =>{
+        socket.on('Message', Msg =>{
             console.log(Msg);
             setChat([...chat,Msg])
         })
@@ -29,19 +29,19 @@ function Chat(){
         setState({...state, [e.target.name]: e.target.value})
     }
 
-    const onMsgSubmit=(e)=>{
+    const onMsgSubmit=e=>{
         e.preventDefault();
         const {Msg} = state;
         socket.emit('Message', {Msg});
         setState({Msg:''})
     }
-    const renderChat=(Msg)=>{
-        return chat.map((Msg)=>(
-            <p className="chat_message">
+    const renderChat=()=>{
+        return chat.map(({Msg}, index)=>(
+            <div key={index} className="chat_message">
                 <span className='chat_name'>tomisin</span>
-                ${Msg}
+                {Msg}
                 <span className='chat_timestamp'>{new Date().toUTCString()}</span>
-            </p>
+            </div>
         ))
     }
 
